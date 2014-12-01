341be6070848ae3d68f52ffc3734f3d7e4be179f
gninjava <gninjava@gmail.com>
2014-12-01 22:04:57 +0200

    javadoc

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -4,49 +4,86 @@ import java.lang.Object;
 import java.util.*;
 
 /**
- * Created by Ninjava on 14.11.2014.
+ * @author Ninjava
+ * @see Collection
  */
 
 public class MyArrayList<T> implements List<T> {
 
+    /**
+     * The size of the ArrayList
+     * */
     private int size = 0;
+
+    /**
+     * The array buffer. Buffer into which the element of the ArrayList are stored.
+     * */
     private Object[] data;
 
+    /**
+     * ArrayList constructor without params. You have to use this type of constructor if you want to create default capacity list.
+     * */
     public MyArrayList() {
         data = new Object[10];
     }
 
+    /**
+     * ArrayList constructor with params. You have to use this type if you want to set another length of list.
+     *
+     * @param length the initial length of list.
+     * */
     public MyArrayList(int length) {
         data = new Object[length];
     }
 
+    /**
+     * Returns the number of elements of the list.
+     *
+     * @return the number of elements of the list.
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Returns true if this list contains no elements.
+     *
+     * @return true if this list contains no elements.
+     */
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Returns <tt>true</tt> if this list contains given element.
+     *
+     * @param o the element which have to check in list.
+     * @return true if the list contains the given element.
+     */
     @Override
     public boolean contains(Object o) {
         return indexOf(o) >= 0;
     }
 
+    /**
+     * Returns the iterator over the elements in this list.
+     *
+     * @return the iterator over the elements in this list.
+     */
     @Override
     public Iterator<T> iterator() {
         return new Itr();
     }
 
+    /**
+     * Returns the array of all elements which are in the list.
+     *
+     * @return the array of all elements which are in the list.
+     */
     @Override
     public Object[] toArray() {
-        /*Object[] toArray = new Object[size];
-        for (int i = 0; i < size; i++) {
-            toArray[i] = data[i];
-        }
-        return toArray;*/
         return Arrays.copyOf(data, size);
     }
 
@@ -55,6 +92,12 @@ public class MyArrayList<T> implements List<T> {
         return null;
     }
 
+    /**
+     * Returns <tt>true</tt>  if the given element has been successfully added.
+     *
+     * @param t the element to be added of this list.
+     * @return true if the given element has been successfully added.
+     */
     @Override
     public boolean add(T t) {
         if (size >= 10) {
@@ -65,6 +108,12 @@ public class MyArrayList<T> implements List<T> {
         return true;
     }
 
+    /**
+     * Returns <tt>true</tt> if the given element has been successfully removed from this list.
+     *
+     * @param o the element to be removed.
+     * @return true if the given element has been successfully removed from this list.
+     */
     @Override
     public boolean remove(Object o) {
         for (int i = 0; i < size; i++) {
@@ -76,6 +125,12 @@ public class MyArrayList<T> implements List<T> {
         return false;
     }
 
+    /**
+     * Return <tt>true</tt> if this list contains all of the elements of given collection.
+     *
+     * @param c collection to be checked.
+     * @return true if this list contains all of the elements of given collection.
+     */
     @Override
     public boolean containsAll(Collection<?> c) {
         Object[] newArray = c.toArray();
@@ -90,6 +145,12 @@ public class MyArrayList<T> implements List<T> {
         return false;
     }
 
+    /**
+     * Returns <tt>true</tt> if all of the elements of given collection has been successfully added to this list.
+     *
+     * @param c collection to be added.
+     * @return true if all of the elements of given collection has been successfully added to this list.
+     */
     @Override
     public boolean addAll(Collection<? extends T> c) {
         Object[] newArray = c.toArray();
@@ -99,6 +160,13 @@ public class MyArrayList<T> implements List<T> {
         return newSize != 0 ? true : false;
     }
 
+    /**
+     * Returns <tt>true</tt> if all of the elements of given collection has been successfully added to this list by index.
+     *
+     * @param index index by which the collection to be added to this list.
+     * @param c collection to be added.
+     * @return true if all of the elements of given collection has been successfully added to this list by index.
+     */
     @Override
     public boolean addAll(int index, Collection<? extends T> c) {
         Object[] newArray = c.toArray();
@@ -109,6 +177,12 @@ public class MyArrayList<T> implements List<T> {
         return true;
     }
 
+    /**
+     * Returns <tt>true</tt> if all of the elements of given collection has been successfully removed from this list.
+     *
+     * @param c collection to be removed.
+     * @return true if all of the elements of given collection has been successfully removed from this list.
+     */
     @Override
     public boolean removeAll(Collection<?> c) {
         Object[] newArray = c.toArray();
@@ -130,6 +204,9 @@ public class MyArrayList<T> implements List<T> {
         return true;
     }
 
+    /**
+     * Removes all elements from this list.
+     */
     @Override
     public void clear() {
         for (int i = 0; i < size; i++) {
@@ -138,15 +215,35 @@ public class MyArrayList<T> implements List<T> {
         size = 0;
     }
 
+    /**
+     * Returns the element of the list by index.
+     *
+     * @param index index of getting element.
+     * @return the element by the given index which is in range of the list indexes.
+     */
     @Override
     public T get(int index) {
         return indexCheck(index) ? (T) data[index] : null;
     }
 
+    /**
+     * Returns <tt>true</tt> if the given index is in range of the list indexes.
+     *
+     * @param index index to be checked.
+     * @return true if the given index is in range of the list indexes.
+     */
     public boolean indexCheck(int index) {
         return (index >= 0 && index <= size) ? true : false;
     }
 
+    /**
+     * Replaces the element at the specified position in this list with
+     * the specified element.
+     *
+     * @param index index of the element to replace.
+     * @param element element to be stored at specified position.
+     * @return the element previously at the specified position.
+     */
     @Override
     public T set(int index, T element) {
         if (indexCheck(index)) {
@@ -157,6 +254,13 @@ public class MyArrayList<T> implements List<T> {
         return null;
     }
 
+    /**
+     * Inserts the specified element at the specified position in this
+     * list.
+     *
+     * @param index index at which the specified element is to be inserted.
+     * @param element element to be inserted.
+     */
     @Override
     public void add(int index, T element) {
         Object[] newArray = new Object[size + 1];
@@ -171,6 +275,13 @@ public class MyArrayList<T> implements List<T> {
         size++;
     }
 
+    /**
+     * Removes the element of specified position in this list.
+     *
+     * @param index index of the element to be removed.
+     * @return the element that was previously removed from the list.
+     * @throws java.lang.IndexOutOfBoundsException
+     */
     @Override
     public T remove(int index) {
         if (index < 0 || index > size) {
@@ -187,6 +298,13 @@ public class MyArrayList<T> implements List<T> {
         return oldValue;
     }
 
+    /**
+     * Returns the index of the first occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     *
+     * @param o the element which index will be returned.
+     * @return the index of the given element.
+     */
     @Override
     public int indexOf(Object o) {
         for (int i = 0; i < size; i++) {
@@ -197,6 +315,13 @@ public class MyArrayList<T> implements List<T> {
         return -1;
     }
 
+    /**
+     * Returns the index of the last occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     *
+     * @param o the element which index will be returned.
+     * @return the index of the given element.
+     */
     @Override
     public int lastIndexOf(Object o) {
         for (int i = size - 1; i >= 0; i++) {
@@ -358,6 +483,9 @@ public class MyArrayList<T> implements List<T> {
         return (List<T>) Arrays.asList(subList);
     }
 
+    /**
+     * Trims the capacity of the list to the list's current size.
+     */
     public boolean trimToSize() {
         if(data.length > size) {
             Arrays.copyOf(data, size);
